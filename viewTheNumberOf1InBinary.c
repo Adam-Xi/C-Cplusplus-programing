e01a9fcd3618744e16d7d1a0baa9aebe1ad5f2f6
Adam-Xi <321041835@qq.com>
2019-06-14 11:08:25 +0800

    二分查找

---
+++ viewTheNumberOf1InBinary.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<Windows.h>
+#pragma warning (disable:4996)
+/*
+int count_one_bits(unsigned int value)
+{
+	int count = 0;
+	while (value){
+		if (value % 2 == 1){
+			count++;
+		}
+		value /= 2;
+	}
+	return count;
+}
+*/
+/*
+//在32为系统下，需判断32次
+int count_one_bits(int value)
+{
+	int count = 0;
+	int c = 0;
+	while (c < 32){
+		if (value & 1){
+			count++;
+		}
+		value >>= 1;
+		c++;
+	}
+	return count;
+}
+*/
+//每一次循环就会判断出来一个1
+int count_one_bits(unsigned int value)
+{
+	int count = 0;
+	while (value){
+		count++;
+		value &= (value - 1);
+	}
+	return count;
+}
+int main()
+{
+	int number = 0;
+	int count = 0;
+	printf("请输入一个数用以计算其二进制中1的个数：");
+	scanf("%d", &number);
+	
+	count=count_one_bits(number);
+	printf("%d", count);
+	
+	system("pause");
+	return 0;
+}
