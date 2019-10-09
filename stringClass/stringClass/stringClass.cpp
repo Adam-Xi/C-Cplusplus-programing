24ebd24d53871f55e51c9d34ae9ac30d66344ad7
Adam-Xi <321041835@qq.com>
2019-10-09 23:48:19 +0800

    string类的模拟实现

--- stringClass/stringClass/stringClass.cpp
+++ stringClass/stringClass/stringClass.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 #include<assert.h>
 
-
 class String
 {
 public:
@@ -23,12 +22,6 @@ public:
 		String tmp(str);
 		this->Swap(tmp);
 	}
-	void Swap(String& str)  //交换对象中的资源
-	{
-		swap(_str, str._str);
-		swap(_capacity, str._capacity);
-		swap(_size, str._size);
-	}
 	~String()  //析构函数
 	{
 		if (_str)
@@ -39,7 +32,7 @@ public:
 			_size = 0;
 		}
 	}
-	String& operator=(String str)
+	String& operator=(String str)  //赋值运算符重载 
 	{
 		this->Swap(str);
 		return *this;
@@ -52,7 +45,7 @@ public:
 	{
 		return _str + _size;
 	}
-	String& operator+=(char c)
+	void PushBack(char c)  //尾插一个字符c
 	{
 		if (_size == _capacity)
 		{
@@ -60,6 +53,41 @@ public:
 		}
 		_str[_size++] = c;
 		_str[_size] = '\0';
+	}
+	String& operator+=(char c)
+	{
+		PushBack(c);
+		return *this;
+	}
+	void Append(const char* str)  //尾接一个字符串
+	{
+		if ((_size + strlen(str)) >= _capacity)
+		{
+			Reserve(_capacity + strlen(str));
+		}
+		strcat(_str, str);
+		_size += strlen(str);
+		_str[_size] = '\0';
+	}
+	String& operator+=(const char* str)
+	{
+		Append(str);
+		return *this;
+	}
+	void Clear()  //清空
+	{
+		_size = 0;
+		_str[size] = '\0';
+	}
+	void Swap(String& str)  //交换对象中的资源
+	{
+		swap(_str, str._str);
+		swap(_capacity, str._capacity);
+		swap(_size, str._size);
+	}
+	const char* C_Str()const
+	{
+		return _str;
 	}
 	String& Reserve(size_t newCapacity)
 	{
@@ -73,7 +101,73 @@ public:
 			_capacity = newCapacity;
 		}
 	}
+	String& Resize(size_t newSize, char c = '\0')
+	{
+		if (newSize > _size)
+		{
+			if (newSize > _capacity)
+			{
+				Reverse(newSize);
+			}
+			memset(_str + _size, c, newSize - _size);
+		}
+		_size = newSize;
+		_str[newSize] = '\0';
+	}
+	char& operator[](size_t index)
+	{
 
+	}
+	const char& operator[](size_t index)const
+	{
+
+	}
+	
+	bool operator<(const String& s)
+	{
+
+	}
+	bool operator<=(const String& s)
+	{
+
+	}
+	bool operator>(const String& s)
+	{
+
+	}
+	bool operator>=(const String& s)
+	{
+
+	}
+	bool operator==(const String& s)
+	{
+		
+	}
+	bool operator!=(const String& s)
+	{
+
+	}
+
+	size_t Find(char c, size_t pos = 0)const
+	{
+
+	}
+	size_t Find(const char* s, size_t pos = 0)const
+	{
+
+	}
+	String& Insert(size_t pos, char c)
+	{
+
+	}
+	String& Insert(size_t pos, const char* str)
+	{
+
+	}
+	String& Erase(size_t pos, size_t len)
+	{
+
+	}
 
 
 
